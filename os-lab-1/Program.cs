a0738893b18a283b93b6d5e6f838a77b52031c3f
matt <matvey.serdukov@gmail.com>
2021-02-23 22:19:42 +0300

    lab 1: main loop improved

--- os-lab-1/Program.cs
+++ os-lab-1/Program.cs
@@ -6,16 +6,10 @@ namespace os_lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Drives info");
-            Console.WriteLine("2. Files");
-            Console.WriteLine("3. JSON");
-            Console.WriteLine("4. XML");
-            Console.WriteLine("5. Zip archives");
-            Console.WriteLine("Please choose task (1-5):");
-
             while (true)
             {
-                var choice = Int32.Parse(Console.ReadLine());
+                PrintOptions();
+                Int32.TryParse(Console.ReadLine(), out var choice);
                 switch (choice)
                 {
                     case 1:
@@ -33,10 +27,26 @@ namespace os_lab_1
                     case 5:
                         Zip.Execute();
                         break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
                     default:
                         break;
                 }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
+        }
+
+        private static void PrintOptions()
+        {
+            Console.WriteLine("1. Drives info");
+            Console.WriteLine("2. Files");
+            Console.WriteLine("3. JSON");
+            Console.WriteLine("4. XML");
+            Console.WriteLine("5. Zip archives");
+            Console.WriteLine("Choose task (1-5) or quit(0):");
         }
     }
 }
