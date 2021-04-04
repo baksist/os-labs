05ea2981fce84c5b8bdeb83a2cc9615135c1ad96
matt <matvey.serdukov@gmail.com>
2021-04-04 20:08:36 +0300

    lab 3 finished: conditions check outside threads & reworked key press

--- os-lab-3/Program.cs
+++ os-lab-3/Program.cs
@@ -9,12 +9,15 @@ namespace os_lab_3
     class Program
     {
         private const int producers = 3;
+        private const int producersTimeout = 300;
+
         private const int consumers = 2;
+        private const int consumersTimeout = 500;
+        
         private const int capacity = 200;
-        private static AutoResetEvent produce_handle = new AutoResetEvent(true);
-        private static AutoResetEvent consume_handle = new AutoResetEvent(true);
 
         private static bool stop_producers = false;
+        private static bool pause_producers = false;
 
         private static BlockingCollection<int> queue = new BlockingCollection<int>(capacity);
         
@@ -23,56 +26,43 @@ namespace os_lab_3
             var prod_threads = new Thread[producers];
             var cons_threads = new Thread[consumers];
 
-            /*for (var i = 0; i < producers; i++)
-            {
-                produce_handles[i] = new AutoResetEvent(true);
-            }*/
             for (var i = 0; i < producers; i++)
             {
-                prod_threads[i] = new Thread(Produce){Name = $"Producer {i}"};
+                prod_threads[i] = new Thread(Produce){Name = $"Producer {i + 1}"};
                 prod_threads[i].Start();
             }
 
             for (var i = 0; i < consumers; i++)
             {
-                cons_threads[i] = new Thread(Consume){Name = $"Consumer {i}"};
+                cons_threads[i] = new Thread(Consume){Name = $"Consumer {i + 1}"};
                 cons_threads[i].Start();
             }
 
-            /*/*char c = Char.MaxValue;
-            while (c != 'q')
-                c = Console.ReadKey().KeyChar;#1#
-            Thread.Sleep(10000);
-            stop_producers = true;*/
-
-            while (true)
+            do
             {
-                var c = Console.Read();
-                if (c == 'q')
-                    break;
-            }
+                while (!Console.KeyAvailable)
+                {
+                    if (queue.Count >= 100)
+                        pause_producers = true;
+                    if (queue.Count <= 80)
+                        pause_producers = false;
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Q);
 
             stop_producers = true;
-
         }
 
-        // TODO: pause all producer threads on condition
         private static void Produce()
         {
             var rnd = new Random();
 
             while (true)
             {
-                queue.Add(rnd.Next(1,100));
-                Console.WriteLine($"{queue.Count}: element added by {Thread.CurrentThread.Name}");
-                Thread.Sleep(300);
-                if (queue.Count >= 100)
+                if (!pause_producers)
                 {
-                    
-                    // var index = Thread.CurrentThread.Name.Last() - 48;
-                    // Console.WriteLine(index);
-                    Console.WriteLine($"Too many elements, {Thread.CurrentThread.Name} sleeps");
-                    produce_handle.WaitOne();
+                    queue.Add(rnd.Next(1, 100));
+                    Console.WriteLine($"{queue.Count}: element added by {Thread.CurrentThread.Name}");
+                    Thread.Sleep(producersTimeout);
                 }
 
                 if (stop_producers)
@@ -80,24 +70,16 @@ namespace os_lab_3
             }
         }
 
-        // TODO: implement wait when queue is empty
         private static void Consume()
         {
             while (true)
             {
                 var n = queue.Take();
                 Console.WriteLine($"{queue.Count}: element taken by {Thread.CurrentThread.Name}");
-                Thread.Sleep(500);
-                if (queue.Count <= 80)
-                {
-                    produce_handle.Set();
-                }
-
-                if (queue.Count >= 95)
-                    consume_handle.WaitOne();
+                Thread.Sleep(consumersTimeout);
 
                 if (queue.Count <= 0)
-                    break;
+                    return;
             }
         }
     }
