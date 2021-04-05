6dcc6ca351296b7fa8e0b4513eb543c8f830ee7d
Matthew <matvey.serdukov@gmail.com>
2021-04-05 09:44:24 +0300

    lab 5: start

---
+++ os-lab-5/ProcessInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace os_lab_5
+{
+    public class ProcessInfo
+    {
+        public Process _process;
+        private int quant;
+        public bool IsReady { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsIdle { get; set; }
+
+        public ProcessInfo(string path)
+        {
+            _process = new Process{StartInfo = new ProcessStartInfo(path)};
+            IsReady = true;
+            IsRunning = false;
+            IsIdle = false;
+            _process.Start();
+        }
+    }
+}
---
+++ os-lab-5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace os_lab_5
+{
+    class Program
+    {
+        private const string ModulePath = "C:\\Windows\\system32\\mspaint.exe";
+        private const int ProcessNumber = 5;
+        
+        static void Main(string[] args)
+        {
+            var ProcList = new List<ProcessInfo>();
+            try
+            {
+                for (var i = 0; i < ProcessNumber; i++)
+                {
+                    ProcList.Add(new ProcessInfo(ModulePath));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
